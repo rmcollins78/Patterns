5faeb58a2adc4f673c39ced95b55ee18994fa3c8
rmcollins78 <rm.collins78@gmail.com>
2026-08-11 09:54:48 -0500

    Update keep-alive.js

--- netlify/functions/keep-alive.js
+++ netlify/functions/keep-alive.js
@@ -1,14 +1,17 @@
 // @netlify-schedule: @daily
 const https = require('https');
+
 exports.handler = async function() {
-  const url = new URL(process.env.SUPABASE_URL + '/rest/v1/cycle_settings?select=id&limit=1');
   return new Promise((resolve) => {
     const req = https.request({
-      hostname: url.hostname,
-      path: url.pathname + url.search,
-      headers: {'apikey': process.env.SUPABASE_ANON_KEY, 'Authorization': 'Bearer ' + process.env.SUPABASE_ANON_KEY}
-    }, () => resolve({statusCode:200,body:'ok'}));
-    req.on('error', () => resolve({statusCode:500,body:'error'}));
+      hostname: 'zslmaqrvsebxhtjirqgf.supabase.co',
+      path: '/rest/v1/cycle_settings?select=id&limit=1',
+      headers: {
+        'apikey': process.env.SUPABASE_ANON_KEY,
+        'Authorization': 'Bearer ' + process.env.SUPABASE_ANON_KEY
+      }
+    }, () => resolve({ statusCode: 200, body: 'ok' }));
+    req.on('error', () => resolve({ statusCode: 500, body: 'error' }));
     req.end();
   });
 };
